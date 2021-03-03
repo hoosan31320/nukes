0d287ce20761b9e099b623cb3070e73af66c1cd8
hoosan31320 <hoos31320@gmail.com>
2021-03-03 20:50:51 +0700

    useTabs

--- src/index.js
+++ src/index.js
@@ -1,55 +1,39 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  } 
+]
 
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item} !</h1>
-        <h2>Start editng to see more magic happen!</h2>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if ( !allTabs || !Array.isArray(allTabs) ) {
+    return;
   }
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  }
 }
 
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+};
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<AppUgly />, rootElement);
-
-// const App = () => {
-//   const [item, setItem] = useState(1);
-//   const incrementItem = () => setItem(item + 1);
-//   const decrementItem = () => setItem(item - 1);
-//   return (
-//     <div className="App">
-//       <h1>Hello {item}</h1>
-//       <h2>Start editng to see more magic happen!</h2>
-//       <button onClick={incrementItem}>Increment</button>
-//       <button onClick={decrementItem}>Decrement</button>
-//     </div>
-//   );
-// };
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, rootElement);
